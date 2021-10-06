2ae109ac38c709f9ebedced5b1288468572e14fb
azhan14 <azhanpatel77@gmail.com>
2021-10-06 17:50:15 +0530

    Unit 3 Room class

---
+++ JavaLabExercise/src/com/neosoft/unit3/Room.java
@@ -0,0 +1,41 @@
+package com.neosoft.unit3;
+
+public class Room {
+	
+	int roomNo;
+	String roomType;
+	double roomArea;
+	boolean ACmachine;
+	
+	void setData(int No, String Type, double Area, boolean AC) {
+		this.roomNo = No;
+		this.roomType = Type;
+		this.roomArea = Area;
+		this.ACmachine = AC;
+	}
+	
+	void displayData() {
+		System.out.println("Room No: "+roomNo);
+		System.out.println("Room Type: "+roomType);
+		System.out.println("Room Area: "+roomArea);
+		System.out.println("AC Present: "+ACmachine);
+	}
+
+	public static void main(String[] args) {
+		Room room1 = new Room();
+		room1.setData(1, "Standard", 20.0, false);
+		room1.displayData();
+		System.out.println("=======================");
+		
+		Room room2 = new Room();
+		room2.setData(2, "Deluxe", 28.9, true);
+		room2.displayData();
+		System.out.println("=======================");
+		
+		Room room3 = new Room();
+		room3.setData(3, "Suite", 35.9, true);
+		room3.displayData();
+
+	}
+
+}
