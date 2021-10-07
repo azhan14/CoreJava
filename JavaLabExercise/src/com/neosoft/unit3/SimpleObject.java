c9bcad1b318792753480ec3bbebb78f711cca9a2
azhan14 <azhanpatel77@gmail.com>
2021-10-07 12:46:50 +0530

    Unit 3 update

---
+++ JavaLabExercise/src/com/neosoft/unit3/SimpleObject.java
@@ -0,0 +1,14 @@
+package com.neosoft.unit3;
+
+public class SimpleObject {
+	
+	// Constructor
+	SimpleObject(){
+		System.out.println("This is a constructor msg.");
+	}
+	
+	public static void main(String[] args) {
+		SimpleObject obj = new SimpleObject();
+	}
+
+}
