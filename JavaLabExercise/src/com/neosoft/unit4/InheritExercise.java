d3d7dadd1b55d9e6212e9c887e686c9d7efdafe7
azhan14 <azhanpatel77@gmail.com>
2021-10-11 17:36:55 +0530

    unit 4 part 2

---
+++ JavaLabExercise/src/com/neosoft/unit4/InheritExercise.java
@@ -0,0 +1,24 @@
+package com.neosoft.unit4;
+
+public class InheritExercise {
+
+	public static void main(String[] args) {
+		Inherit obj = new Inherit(); // object of the last child in inheritance.
+		System.out.println(obj.in);		// can access all the variables from parent class
+		System.out.println(obj.b);
+		System.out.println(obj.a);
+	}
+
+}
+//Parent class
+class A{
+	int a = 10;
+}
+// Child class of A
+class B extends A{
+	int b = 20;
+}
+// Child class of B
+class Inherit extends B{
+	int in = 30;
+}
---
+++ JavaLabExercise/src/com/neosoft/unit4/SuperEx.java
@@ -0,0 +1,35 @@
+package com.neosoft.unit4;
+
+public class SuperEx {
+
+	public static void main(String[] args) {
+		OP op = new OP();
+		op.Category();
+
+	}
+
+}
+
+class Anime{
+	String hr = "===========";
+	Anime(){
+		System.out.println("Anime..");
+	}
+	
+	void Category() {
+		System.out.println("Category..");
+	}
+}
+
+class OP extends Anime {
+	OP(){
+		super(); // calling parent contructor.
+		System.out.println("One Piece..");
+		System.out.println(super.hr);	// using parent variable
+	}
+	
+	void Category() {
+		super.Category(); //calling parent method
+		System.out.println("Adventure..");
+	}
+}
