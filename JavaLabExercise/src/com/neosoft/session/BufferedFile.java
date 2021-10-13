c5b2fe17064d211013c2c60e0626b181b2450a6c
azhan14 <azhanpatel77@gmail.com>
2021-10-13 19:32:19 +0530

    Buffered Update

--- JavaLabExercise/src/com/neosoft/session/BufferedFile.java
+++ JavaLabExercise/src/com/neosoft/session/BufferedFile.java
@@ -19,20 +19,45 @@ public class BufferedFile {
 			try {
 				reader = new BufferedReader(new FileReader(file));
 				File testCopy = new File("resources/testCopy.txt");
-				BufferedWriter br1 = new BufferedWriter(new FileWriter(testCopy,true));
-				String currentLine = reader.readLine();
-				while(currentLine != null) {
-					br1.write(currentLine);
-					br1.newLine();
-					lineCount++;
-					String[] words = currentLine.split(" ");
-	                wordCount = wordCount + words.length;
-	                currentLine = reader.readLine();
+				BufferedReader br2 = new BufferedReader(new FileReader(testCopy));
+				String line = br2.readLine();
+				int flag = 0;
+				while(line != null) {
+					String[] word = line.split(" ");
+					for(String s: word) {
+						if(s.toLowerCase().equals("java")) {
+							flag = 1;
+							break;
+						}
+						else {
+							flag = 0;
+						}
+					}
+					line = br2.readLine();
+							
 				}
-				System.out.println("Line Count: "+lineCount);
-				System.out.println("Word Count: "+wordCount);
-				reader.close();
-				br1.close();
+				br2.close();
+				if(flag == 1) {
+					BufferedWriter br1 = new BufferedWriter(new FileWriter(testCopy,true));
+					
+					String currentLine = reader.readLine();
+					while(currentLine != null) {
+						br1.newLine();
+						br1.write(currentLine);
+						lineCount++;
+						String[] words = currentLine.split(" ");
+						
+		                wordCount = wordCount + words.length;
+		                currentLine = reader.readLine();
+					}
+					System.out.println("Line Count: "+lineCount);
+					System.out.println("Word Count: "+wordCount);
+					reader.close();
+					br1.close();
+				}
+				else {
+					System.out.println("File does not contain java. Therefore can't write");
+				}
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
