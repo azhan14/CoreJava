b5bb26d39ba7e36f12180f3238101e9952a2f2ae
azhan14 <azhanpatel77@gmail.com>
2021-11-12 22:52:13 +0530

    Date Query

--- JavaLabExercise/src/com/neosoft/JDBCTask/DateTask.java
+++ JavaLabExercise/src/com/neosoft/JDBCTask/DateTask.java
@@ -51,15 +51,73 @@ public class DateTask {
 			System.out.println("Give 5% increment to employees who are here since last 3 years?");
 			System.out.println("----------------------");
 			
-			String selectQuery2 = "SELECT *,DATEDIFF(CURDATE(),HIREDATE) AS NOD FROM EMP WHERE DATEDIFF(CURDATE(),HIREDATE) = (SELECT MAX(DATEDIFF(CURDATE(),HIREDATE)) FROM EMP);";
+			String selectQuery2 = "SELECT *,sal*1.05 as newsal FROM emp WHERE TIMESTAMPDIFF(YEAR,hiredate,CURDATE()) > 3;";
 			rs = st.executeQuery(selectQuery2);
-			System.out.println("empno \t ename \t job \t hiredate \t sal \t NOD");
+			System.out.println("empno \t ename \t job \t\t sal \t newsal");
 			while(rs.next()) {
-				System.out.println(rs.getInt("empno")+"\t"+rs.getString("ename")+"\t"+rs.getString("job")
-				+"\t"+rs.getDate("hiredate")+"\t"+rs.getBigDecimal("sal")+"\t"+rs.getBigDecimal("NOD"));
+				System.out.println(rs.getInt("empno")+"\t"+rs.getString("ename")+"\t"+rs.getString("job")+
+						"   \t"+rs.getBigDecimal("sal")+"\t"+rs.getBigDecimal("newsal"));
 			}
 			rs.close();
 			
+			// Date Query 3
+			System.out.println("----------------------");
+			System.out.println("Give 7% increment to employees who are here since last 5 years and salary < 2000?");
+			System.out.println("----------------------");
+			
+			String selectQuery3 = "SELECT *,sal*1.07 as newsal FROM emp WHERE TIMESTAMPDIFF(YEAR,hiredate,CURDATE()) > 5 AND sal < 2000;";
+			rs = st.executeQuery(selectQuery3);
+			System.out.println("empno \t ename \t job \t\t sal \t newsal");
+			while(rs.next()) {
+				System.out.println(rs.getInt("empno")+"\t"+rs.getString("ename")+"\t"+rs.getString("job")+
+						"   \t"+rs.getBigDecimal("sal")+"\t"+rs.getBigDecimal("newsal"));
+			}
+			rs.close();
+			
+			// Date Query 4
+			System.out.println("----------------------");
+			System.out.println("Display yearly report which shows year wise no. of joinings?");
+			System.out.println("----------------------");
+			
+			String selectQuery4 = "SELECT DATE_FORMAT(hiredate,\"%Y\") AS year,COUNT(*) AS no_of_joining FROM emp GROUP BY year;";
+			rs = st.executeQuery(selectQuery4);
+			System.out.println("year \tno_of_joining");
+			while(rs.next()) {
+				System.out.println(rs.getString("year")+"\t"+rs.getInt("no_of_joining"));
+			}
+			rs.close();
+			
+			// Date Query 5
+			System.out.println("----------------------");
+			System.out.println("Display monthly report which shows month wise no. of joinings?");
+			System.out.println("----------------------");
+			
+			String selectQuery5 = "SELECT DATE_FORMAT(hiredate, \"%M\") AS month,COUNT(*) AS no_of_joining FROM emp GROUP BY month;";
+			rs = st.executeQuery(selectQuery5);
+			System.out.println("no_of_joining \t month");
+			while(rs.next()) {
+				System.out.println(rs.getInt("no_of_joining")+"\t"+rs.getString("month"));
+			}
+			rs.close();
+			
+			// Date Query 6
+			System.out.println("----------------------");
+			System.out.println("Create a seperate table from existing emp table for emps who have recently joined (i.e. last year).");
+			System.out.println("----------------------");
+			
+//			String createQuery = "CREATE TABLE new_emp SELECT * FROM emp WHERE YEAR(hiredate)>=YEAR('2021-01-01');";
+//			System.out.println("Table Creation: "+st.executeUpdate(createQuery));
+			
+			
+			String selectQuery6 = "SELECT * FROM new_emp;";
+			rs = st.executeQuery(selectQuery6);
+			System.out.println("empno \t ename \t job \t mgr \t hiredate \t sal \t deptno");
+			while(rs.next()) {
+				System.out.println(rs.getInt("empno")+"\t"+rs.getString("ename")
+				+"\t"+rs.getString("job")+"\t"+rs.getInt("mgr")
+				+"\t"+rs.getDate("hiredate")+"\t"+rs.getInt("sal")+"\t"+rs.getInt("deptno"));
+			}
+			rs.close();
 		} catch(Exception e) {
 			e.printStackTrace();
 		} finally {
